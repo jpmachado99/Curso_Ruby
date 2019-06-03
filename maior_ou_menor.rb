c280a8d976ba7ee62d033d855a9549458741fa4f
João Pedro Capelo Machado <joao.machado5@fatec.sp.gov.br>
2019-06-03 16:03:37 -0300

    Aula 4

--- maior_ou_menor.rb
+++ maior_ou_menor.rb
@@ -5,15 +5,16 @@ def boas_vindas
 	puts "\nInsira seu nome:" 
 	nome = gets
 
-	puts "\nComeçaremos o jogo para você, " + nome 	
+	puts "\nComeçaremos o jogo para você, #{nome}"	
 end
 def sorteio
 	puts "Escolhendo um número secreto de 0 a 10..."
 	sorteado = 7
 end
-def pede_numero(tentativas, limite_de_tentativas)
-	puts "\nTentativa " + tentativas.to_s + " de " + limite_de_tentativas.to_s
-	chute = gets
+def pede_numero(chutes, tentativas, limite_de_tentativas)
+	puts "\nVocê chutou: #{chutes}"
+	puts "Tentativa #{tentativas} de #{limite_de_tentativas}"
+	chute = gets.strip
 end
 def verifica(numero, chute)
 	acertou = chute.to_i == numero
@@ -35,10 +36,12 @@ boas_vindas
 numero = sorteio
 
 limite_de_tentativas = 5
+chutes = []
 
 #tentativas = 1
 
 for tentativas in 1..limite_de_tentativas
-	chute = pede_numero tentativas, limite_de_tentativas
+	chute = pede_numero(chutes, tentativas, limite_de_tentativas)
+	chutes << chute
 	break if verifica numero, chute
 end
